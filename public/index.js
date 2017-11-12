e3451d5f88bb63461183547a41e79884724646c9
timmyyeol <kseana@mail.ru>
2017-11-12 17:45:55 +0300

    help

---
+++ public/index.js
@@ -0,0 +1,48 @@
+﻿const setResult=result=>document.getElementById('result').innerHTML=`Результат: ` + result;
+
+const addition=(a,b)=>{
+	const promise = fetch(`http://localhost:3000/addition?a=${a}&b=${b}`);
+	promise.then(res => {
+		if (res.status !== 200) {
+			console.log(`Problem. Status code: ${res.status}`);
+			return;
+		}
+		res.json().then(({result}) => setResult(result));
+	}).catch(err => console.log(`Fetch error: ${err}`))
+};
+
+
+const subtraction=(a,b)=>{
+	const promise = fetch(`http://localhost:3000/subtraction?a=${a}&b=${b}`);
+	promise.then(res => {
+		if (res.status !== 200) {
+			console.log(`Problem. Status code: ${res.status}`);
+			return;
+		}
+		res.json().then(({result}) => setResult(result));
+	}).catch(err => console.log(`Fetch error: ${err}`))
+};
+
+
+const multiplication=(a,b)=>{
+	const promise = fetch(`http://localhost:3000/multiplication?a=${a}&b=${b}`);
+	promise.then(res => {
+		if (res.status !== 200) {
+			console.log(`Problem. Status code: ${res.status}`);
+			return;
+		}
+		res.json().then(({result}) => setResult(result));
+	}).catch(err => console.log(`Fetch error: ${err}`))
+};
+
+
+const division=(a,b)=>{
+	const promise = fetch(`http://localhost:3000/division?a=${a}&b=${b}`);
+	promise.then(res => {
+		if (res.status !== 200) {
+			console.log(`Problem. Status code: ${res.status}`);
+			return;
+		}
+		res.json().then(({result}) => setResult(result));
+	}).catch(err => console.log(`Fetch error: ${err}`))
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+﻿const express = require('express');
+const app = express();
+app.use(express.static(`${__dirname}/public`));
+app.listen(3000, () => console.log('server started'));
+app.get('/addition', (req, res) => {
+	const query = req.query;
+	const { a = 0, b = 0 } = query;
+	res.json({ result: parseInt(a) + parseInt(b)});
+});
+
+app.get('/subtraction', (req, res) => {
+	const query = req.query;
+	const { a = 0, b = 0 } = query;
+	res.json({ result: parseInt(a) - parseInt(b)});
+});
+
+app.get('/multiplication', (req, res) => {
+	const query = req.query;
+	const { a = 0, b = 0 } = query;
+	res.json({ result: parseInt(a) * parseInt(b)});
+});
+
+app.get('/division', (req, res) => {
+	const query = req.query;
+	const { a = 0, b = 0 } = query;
+	res.json({ result: parseInt(a) / parseInt(b)});
+});
